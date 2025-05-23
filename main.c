be66e0a1db7ce0769d189a76fff4ac9769ce49c5
Luca Nasrallah <lucanasrallah8@gmail.com>
2025-05-23 19:55:29 -0400

    Create main.c
    Assignment 1  Main.c file

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>    
+#include <stdlib.h>  
+#include "functions.h"
+//main.c
+int main() {
+    if(argc < 2){
+        fprintf(stdeerr, "Usage: %s <")
+    }
+    
+    return 0; 
+}
+
+
+
