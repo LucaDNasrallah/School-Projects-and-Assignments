315766d029376d153acc1a03a870df30638b6f20
Luca Nasrallah <lucanasrallah8@gmail.com>
2025-05-27 11:01:04 -0400

    Update functions.h

--- 348_A1/functions.h
+++ 348_A1/functions.h
@@ -6,22 +6,29 @@
 #define MAX_LEN 101
 
 typedef struct{
-    int id;
-    char date[MAX_LEN];
-    char type[MAX_LEN];
-    char subtype[MAX_LEN];
-    char description[MAX_LEN];
-    float amount;
+    int id; 
+    char date[MAX_LEN]; // Date for String Example: 2025-05-23
+    char type[MAX_LEN]; // Entry type Either Income or Expense
+    char subtype[MAX_LEN]; // Either Salary or Rent
+    char description[MAX_LEN]; // Short description of the entry
+    float amount; // Amount, can be both postive and negative
 }Entry;
 
+
+//Global Varibales defined in function.c
 extern Entry entries[MAX_ENTRIES]; // This is an Array that Stroes all teh entries. 
 extern int entryCount; // Saved number of Entries being loaded
 
 //Functions declarations
-void loadEntries(char *fileName); // Load data from input file
+int  loadEntries( const char *fileName); // Load data from input 
+void displayMenu(); // Self explanatory 
 void displayEntries(); // Choice 1 - Displays all entries in a table  
-// Missing the other choices 
-// COME BACK THE OTHER CHOICES ONCE THE FIRST WORKS
+
+//Choice 2 -
+//Choice 3 - 
+//Choice 4 - 
+// ... When Choice one works continue 
+
 
 
 void clearScreen(); // Clear Terminal Screen. 
