77780f2768cb00b12916f2a0829ae6595238bc6a
Luca Nasrallah <lucanasrallah8@gmail.com>
2025-05-28 17:11:44 -0400

    Update functions.h

--- 348_A1/functions.h
+++ 348_A1/functions.h
@@ -8,8 +8,8 @@
 typedef struct{
     int id; 
     char date[MAX_LEN]; // Date for String Example: 2025-05-23
-    char type[MAX_LEN]; // Entry type Either Income or Expense
-    char subtype[MAX_LEN]; // Either Salary or Rent
+    char type[MAX_LEN]; // Entry type Either Income, Expenses or Needs
+    char subtype[MAX_LEN]; // Either Needs or Want
     char description[MAX_LEN]; // Short description of the entry
     float amount; // Amount, can be both postive and negative
 }Entry;
@@ -22,14 +22,13 @@ extern int entryCount; // Saved number of Entries being loaded
 //Functions declarations
 int  loadEntries( const char *fileName); // Load data from input 
 void displayMenu(); // Self explanatory 
+
 void displayEntries(); // Choice 1 - Displays all entries in a table  
-
-//Choice 2 -
-//Choice 3 - 
-//Choice 4 - 
-// ... When Choice one works continue 
-
-
+void expenseDistribution(); //Choice 2 - Shows a summary reportof the entries 
+void sortEntries(); //Choice 3 - 
+void addEntry();//Choice 4 - 
+void modifyEntry(); // Choice -5 - 
+void filterByMonth(); // Choice 6 - 
 
 void clearScreen(); // Clear Terminal Screen. 
 
